a8d377eeaa727c3f975cdb1806a0b0e8a65f7d78
Vinícius <vinicius.ferreira@ufpe.br>
2024-07-25 07:05:10 -0300

    Ex1: configuração do projeto

---
+++ m2s10/src/main/java/com/fmt/m2s10/M2s10Application.java
@@ -0,0 +1,13 @@
+package com.fmt.m2s10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s10Application.class, args);
+	}
+
+}
---
+++ m2s10/src/test/java/com/fmt/m2s10/M2s10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmt.m2s10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2s10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
